cccf580eb3c5cf5bdbeb3f62923790db7075082b
rblancobarbara <rblancobarbara@yahoo.com.ar>
2018-03-26 09:58:15 -0400

    Starting Part III

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,19 +7,40 @@ const sortByOptions = {
   'MostReviewed': 'review_count'
 }
 
+getSortByClass(sortByOption) {
+  return this.state.sortBy == sortByOption ? 'active' : '';
+}
+
+/*
+handleSortByChange(sortByOption) {
+  this.setState() = this.state.sortBy;
+}
+*/
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match',
+    };
+  }
+
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
         return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
+
   render() {
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
           <ul>
-            this.renderSortByOptions();
+            <li renderSortByOptions>Best Match</li>
+            <li renderSortByOptions>Highest Rated</li>
+            <li renderSortByOptions>Most Reviewed</li>
           </ul>
         </div>
         <div className="SearchBar-fields">
@@ -27,10 +48,10 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a>"Let's Go"</a>
+          <a>"Let's Go"< /a>
         </div>
       </div>
-    )  
+    )
   }
 }
 
